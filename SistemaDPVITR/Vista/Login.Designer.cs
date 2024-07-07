b9cb7da6feda37b288c4f82aec0b665549aaa0b8
aleexandeer47 <20230633@ricaldone.edu.sv>
2024-07-07 14:13:31 -0600

    Add project files.

---
+++ SistemaDPVITR/Vista/Login.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SistemaDPVITR
+{
+    partial class FLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogin));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pedirCredentxt1 = new System.Windows.Forms.Label();
+            this.PedirCredentxt2 = new System.Windows.Forms.Label();
+            this.PedirCorreotxt = new System.Windows.Forms.Label();
+            this.Contraseñatxt = new System.Windows.Forms.Label();
+            this.CorreotxtBx = new System.Windows.Forms.TextBox();
+            this.ContraseñatxtBx = new System.Windows.Forms.TextBox();
+            this.Ingresarbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(469, 102);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(146, 137);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(221, 69);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(623, 558);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1049, 702);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pedirCredentxt1
+            // 
+            this.pedirCredentxt1.AutoSize = true;
+            this.pedirCredentxt1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pedirCredentxt1.Font = new System.Drawing.Font("Poppins", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pedirCredentxt1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pedirCredentxt1.Location = new System.Drawing.Point(382, 262);
+            this.pedirCredentxt1.Name = "pedirCredentxt1";
+            this.pedirCredentxt1.Size = new System.Drawing.Size(314, 30);
+            this.pedirCredentxt1.TabIndex = 3;
+            this.pedirCredentxt1.Text = "Por favor, Ingrese sus credenciales";
+            // 
+            // PedirCredentxt2
+            // 
+            this.PedirCredentxt2.AutoSize = true;
+            this.PedirCredentxt2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PedirCredentxt2.Font = new System.Drawing.Font("Poppins", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PedirCredentxt2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.PedirCredentxt2.Location = new System.Drawing.Point(474, 292);
+            this.PedirCredentxt2.Name = "PedirCredentxt2";
+            this.PedirCredentxt2.Size = new System.Drawing.Size(131, 30);
+            this.PedirCredentxt2.TabIndex = 4;
+            this.PedirCredentxt2.Text = "para acceder";
+            // 
+            // PedirCorreotxt
+            // 
+            this.PedirCorreotxt.AutoSize = true;
+            this.PedirCorreotxt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PedirCorreotxt.Font = new System.Drawing.Font("Poppins", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PedirCorreotxt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PedirCorreotxt.Location = new System.Drawing.Point(293, 354);
+            this.PedirCorreotxt.Name = "PedirCorreotxt";
+            this.PedirCorreotxt.Size = new System.Drawing.Size(166, 26);
+            this.PedirCorreotxt.TabIndex = 5;
+            this.PedirCorreotxt.Text = "Correo Institucional:";
+            // 
+            // Contraseñatxt
+            // 
+            this.Contraseñatxt.AutoSize = true;
+            this.Contraseñatxt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Contraseñatxt.Font = new System.Drawing.Font("Poppins", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contraseñatxt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Contraseñatxt.Location = new System.Drawing.Point(293, 463);
+            this.Contraseñatxt.Name = "Contraseñatxt";
+            this.Contraseñatxt.Size = new System.Drawing.Size(106, 26);
+            this.Contraseñatxt.TabIndex = 6;
+            this.Contraseñatxt.Text = "Contraseña:";
+            // 
+            // CorreotxtBx
+            // 
+            this.CorreotxtBx.Location = new System.Drawing.Point(298, 398);
+            this.CorreotxtBx.Name = "CorreotxtBx";
+            this.CorreotxtBx.Size = new System.Drawing.Size(500, 22);
+            this.CorreotxtBx.TabIndex = 7;
+            // 
+            // ContraseñatxtBx
+            // 
+            this.ContraseñatxtBx.Location = new System.Drawing.Point(298, 501);
+            this.ContraseñatxtBx.Name = "ContraseñatxtBx";
+            this.ContraseñatxtBx.Size = new System.Drawing.Size(500, 22);
+            this.ContraseñatxtBx.TabIndex = 8;
+            // 
+            // Ingresarbtn
+            // 
+            this.Ingresarbtn.BackColor = System.Drawing.Color.Orange;
+            this.Ingresarbtn.Font = new System.Drawing.Font("Poppins", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ingresarbtn.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Ingresarbtn.Location = new System.Drawing.Point(455, 556);
+            this.Ingresarbtn.Name = "Ingresarbtn";
+            this.Ingresarbtn.Size = new System.Drawing.Size(193, 44);
+            this.Ingresarbtn.TabIndex = 9;
+            this.Ingresarbtn.Text = "Ingresar";
+            this.Ingresarbtn.UseVisualStyleBackColor = false;
+            // 
+            // FLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 693);
+            this.Controls.Add(this.Ingresarbtn);
+            this.Controls.Add(this.ContraseñatxtBx);
+            this.Controls.Add(this.CorreotxtBx);
+            this.Controls.Add(this.Contraseñatxt);
+            this.Controls.Add(this.PedirCorreotxt);
+            this.Controls.Add(this.PedirCredentxt2);
+            this.Controls.Add(this.pedirCredentxt1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FLogin";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label pedirCredentxt1;
+        private System.Windows.Forms.Label PedirCredentxt2;
+        private System.Windows.Forms.Label PedirCorreotxt;
+        private System.Windows.Forms.Label Contraseñatxt;
+        public System.Windows.Forms.TextBox CorreotxtBx;
+        public System.Windows.Forms.TextBox ContraseñatxtBx;
+        public System.Windows.Forms.Button Ingresarbtn;
+        public System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
